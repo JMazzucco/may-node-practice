dfa01a9b4d3d6a58ab6d5b7767ce0c38313de0ba
John Mazzucco <John.Mazzucco@gmail.com>
2016-05-02 14:46:21 -0400

    Write to head and body

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 var http = require("http");
 
-http.createServer(function (request, response){
+var server = http.createServer(function (request, response){
+	//send the HTTP header
+	response.writeHead(200, {'Content-Type': 'text/plain'});
 
-}).listen(8081);
+	//send the response body
+  response.write("<!DOCTYPE "html">");
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
+})
+
+server.listen(8081);
+
+// Console will print the message
+console.log("Server is listening");
